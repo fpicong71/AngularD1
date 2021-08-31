4357450dc9f1ddc707ed765de3a91f032109abed
fpicong71 <fpicong@yahoo.es>
2021-08-31 15:21:06 +0200

    build(*):retos dia 1 Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CabeceraComponent } from './pages/cabecera/cabecera.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CabeceraComponent,
+    MenuComponent,
+    FooterComponent,
+    ContactPageComponent,
+    LoginPageComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
